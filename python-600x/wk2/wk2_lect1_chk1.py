d0c224b1fa107fecce7c276176b060c608d9fd91
Charles Milam <charles.milam@gmail.com>
2016-01-23 02:20:23 -0600

    Add prompting of user for their secret number. Add check for a proper response, with prompt if incorrect. Add algorithm for resolving the secret number.

--- python-600x/wk2/wk2_lect1_chk1.py
+++ python-600x/wk2/wk2_lect1_chk1.py
@@ -1,8 +1,29 @@
 # number guessing game
 secret = int(raw_input('Please think of a number between 0 and 100! '))
-guess = 50
+low = 0
+high = 100
+guess = (low + high) / 2
+guess_msg = "Enter 'h' for too high, 'l' for too low, 'c' for correct: "
+resp = ""
+proper_resps = ['h', 'l', 'c']
 
-while guess != secret:
-    print 'keep guessing'
+while resp != 'c':
+    print 'Is your secret number ' + str(guess) + "?"
+    print
+
+    resp = raw_input(guess_msg)
+    if resp not in proper_resps:
+        print "Please respond with only 'h', 'l' or 'c'"
+        print 'Try again'
+        resp = raw_input(guess_msg)
+        print 
+
+    if resp == 'h':
+        high = guess
+        guess = (low + high) / 2
+    elif resp == 'l':
+        low = guess
+        guess = (low + high) / 2
+
 
 print 'Your secret number is ' + str(guess)
