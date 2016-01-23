f9dbfd26ec60ddad4114690a9c9073338c542fcd
Charles Milam <charles.milam@gmail.com>
2016-01-23 02:32:25 -0600

    Add additional formatting. Place error message in a variable.

--- python-600x/wk2/wk2_lect1_chk1.py
+++ python-600x/wk2/wk2_lect1_chk1.py
@@ -4,6 +4,7 @@ low = 0
 high = 100
 guess = (low + high) / 2
 guess_msg = "Enter 'h' for too high, 'l' for too low, 'c' for correct: "
+error_msg = "Please respond with only 'h', 'l' or 'c' \nTry again"
 resp = ""
 proper_resps = ['h', 'l', 'c']
 
@@ -15,8 +16,8 @@ while resp != 'c':
     resp = raw_input(guess_msg)
     print
     while resp not in proper_resps:
-        print "Please respond with only 'h', 'l' or 'c'"
-        print 'Try again'
+        print
+        print error_msg
         print
         resp = raw_input(guess_msg)
 
@@ -26,6 +27,5 @@ while resp != 'c':
     elif resp == 'l':
         low = guess
         guess = (low + high) / 2
-
 
 print 'Your secret number is ' + str(guess)
