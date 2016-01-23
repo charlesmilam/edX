a490682ad78b7e6fb3172e430c1d94812c45b2a9
Charles Milam <charles.milam@gmail.com>
2016-01-23 01:49:32 -0600

    Basic framework of program laid out. Receive input from user, make check, print success message.

--- python-600x/wk2/wk2_lect1_chk1.py
+++ python-600x/wk2/wk2_lect1_chk1.py
@@ -1,1 +1,8 @@
 # number guessing game
+secret = int(raw_input('Please think of a number between 0 and 100! '))
+guess = 50
+
+while guess != secret:
+    print 'keep guessing'
+
+print 'Your secret number is ' + str(guess)
