cd6ead1408ef867151e86bb10de5e665583099ee
Charles Milam <charles.milam@gmail.com>
2016-01-24 00:23:33 -0600

    Add loop to calculate minimum payment, the monthly unpaid balance, and the new balance over a year period, based on starting values provided by assignment.

---
+++ python-600x/wk2/ps2_p1.py
@@ -0,0 +1,13 @@
+balance = 4213
+annualInterestRate = 0.2
+monthlyPaymentRate = 0.04
+monthly_interest_rate = annualInterestRate / 12.0
+
+for month in range(1, 13):
+    min_payment = balance * monthlyPaymentRate
+    monthly_upaid_bal = balance - min_payment
+    balance = monthly_upaid_bal + (monthly_interest_rate * monthly_upaid_bal)
+
+    print 'Month: ' + str(month)
+    print 'Minimum monthly payment: ' + str(min_payment)
+    print 'Remaining balance: ' + str(balance)
