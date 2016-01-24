62f0a85a60a4ad4cc7be70035c8a44c9c6e5a04f
Charles Milam <charles.milam@gmail.com>
2016-01-24 04:14:22 -0600

    Remove debug statements and test parameters.

--- python-600x/wk2/ps2_p3.py
+++ python-600x/wk2/ps2_p3.py
@@ -3,9 +3,6 @@ annualInterestRate = 0.18
 monthly_interest_rate = annualInterestRate / 12
 lower_bound = balance / 12
 upper_bound = balance * (1 + monthly_interest_rate)**12 / 12.0
-
-count = 0
-# fixed_pay = (lower_bound + upper_bound) / 2
 diff = .01
 
 def test_fixed_pay(payment, bal):
@@ -15,18 +12,14 @@ def test_fixed_pay(payment, bal):
 
     return bal
 
-while count <= 100:
+while True:
     fixed_pay = (lower_bound + upper_bound) / 2
-    print lower_bound, upper_bound, fixed_pay
-    print balance / fixed_pay, test_fixed_pay(fixed_pay, balance)
 
     if abs(test_fixed_pay(fixed_pay, balance)) <= diff:
         print 'Lowest Payment: ' + str(round(fixed_pay, 2))
-        # break
+        break
     elif test_fixed_pay(fixed_pay, balance) > diff:
         lower_bound = fixed_pay
 
     elif test_fixed_pay(fixed_pay, balance) < diff:
         upper_bound = fixed_pay
-
-    count += 1
