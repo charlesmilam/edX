778aeeb5bada1b8a9f0e75069fa2d3a886b56917
Charles Milam <charles.milam@gmail.com>
2016-01-31 10:32:37 -0600

    Add provided helper function fo determining the decay at any given point on the curve.

--- python-600x/wk3/radiation_exposure.py
+++ python-600x/wk3/radiation_exposure.py
@@ -1,3 +1,7 @@
+def f(x):
+    import math
+    return 10*math.e**(math.log(0.5)/5.27 * x)
+
 def radiationExposure(start, stop, step):
     '''
     Computes and returns the amount of radiation exposed
