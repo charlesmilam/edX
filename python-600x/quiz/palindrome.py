4241b8b394533523cbf185114d3dfb73a24c1977
Charles Milam <charles.milam@gmail.com>
2016-02-14 19:03:16 -0600

    Add reversal of string paramater to meet problem requirements, for use in checking if palindrome. Add some basic tests.

--- python-600x/quiz/palindrome.py
+++ python-600x/quiz/palindrome.py
@@ -2,4 +2,17 @@ def isPalindrome(aString):
     '''
     aString: a string
     '''
-    
+    if len(aString) <= 1:
+        return False
+
+    rev_str = ''
+
+    for char in aString:
+        rev_str = char + rev_str
+
+    print 'strs', aString, rev_str
+
+    return aString == rev_str
+
+print 'Should return False: ' + str(isPalindrome(''))
+print 'Shoule return False: ' + str(isPalindrome('a'))
