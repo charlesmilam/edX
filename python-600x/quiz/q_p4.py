cea80bab4edd914c0979783d014d53f7961b3ce7
Charles Milam <charles.milam@gmail.com>
2016-02-14 19:26:04 -0600

    Implement looping through the lists, adding the product of the two numbers from each list index to the overall total, and returning the total.

--- python-600x/quiz/q_p4.py
+++ python-600x/quiz/q_p4.py
@@ -3,4 +3,12 @@ def dotProduct(listA, listB):
     listA: a list of numbers
     listB: a list of numbers of the same length as listA
     '''
-    # Your code here
+    total = 0
+
+    for idx in range(len(listA)):
+        print listA[idx], listB[idx]
+        total += listA[idx] * listB[idx]
+
+    return total
+
+print str(dotProduct([-90, 74, 39, 89, -24, -71, 44, -98], [15, 61, 33, -45, 47, 0, -9, -58]))
