3eeb2bf516623ec2d306cabdc894287526e98035
Charles Milam <charles.milam@gmail.com>
2016-02-15 21:59:04 -0600

    Add the loop through the dict keys, creating a new dict of the intersecting keys.

--- python-600x/quiz/q_p7.py
+++ python-600x/quiz/q_p7.py
@@ -8,9 +8,23 @@ d1 = {1:30, 2:20, 3:30}
 d2 = {1:40, 2:50, 3:60}
 then dict_interdiff(d1, d2) returns ({1: False, 2: False, 3: False}, {})
 '''
+d1 = {1:30, 2:20, 3:30, 5:80}
+d2 = {1:40, 2:50, 3:60, 4:70, 6:90}
+
 def dict_interdiff(d1, d2):
     '''
     d1, d2: dicts whose keys and values are integers
     Returns a tuple of dictionaries according to the instructions above
     '''
-    # Your code here
+    inter_keys = {}
+    inter_ans = {}
+
+    for idx, key in enumerate(d1.keys()):
+        if key in d2.keys():
+            inter_keys[idx] = key
+
+    print inter_keys
+    # for key in inter_keys:
+    #     inter_ans[key] = f(d1[key], d2[key])
+
+dict_interdiff(d1, d2)
