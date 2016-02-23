6ec9335caa246c37fb0c6492eed240b97a9e70f7
Charles Milam <charles.milam@gmail.com>
2016-02-23 07:03:36 -0600

    Add methods for __eq__ and __repr__ per requirements of problem.

--- python-600x/wk6/l11_p4.py
+++ python-600x/wk6/l11_p4.py
@@ -14,3 +14,9 @@ class Coordinate(object):
 
     def __str__(self):
         return '<' + str(self.getX()) + ',' + str(self.getY()) + '>'
+
+    def __eq__(self, other):
+        return (self.getX() == other.getX()) and (self.getY() == other.getY())
+
+    def __repr__(self):
+        return 'Coordinate(%d, %d)' % (self.getX(), self.getY())
