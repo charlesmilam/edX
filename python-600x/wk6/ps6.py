8f5e3505582dd17b28aecebdaf59230aa6b9ec20
Charles Milam <charles.milam@gmail.com>
2016-03-03 06:49:21 -0600

    Add instance attributes to CiphertextMessage init method.

--- python-600x/wk6/ps6.py
+++ python-600x/wk6/ps6.py
@@ -222,7 +222,9 @@ class CiphertextMessage(Message):
             self.message_text (string, determined by input text)
             self.valid_words (list, determined using helper function load_words)
         '''
-        pass #delete this line and replace with your code here
+        Message.__init__(self, text)
+        self.message_text = text
+        self.valid_words = load_words(WORDLIST_FILENAME)
 
     def decrypt_message(self):
         '''
