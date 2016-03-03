7466f83ba1d03cac393fbd2a5e000741164db7bb
Charles Milam <charles.milam@gmail.com>
2016-03-02 19:56:04 -0600

    Add return of shift to get_shift of PlaintextMessage.

--- python-600x/wk6/ps6.py
+++ python-600x/wk6/ps6.py
@@ -148,7 +148,7 @@ class PlaintextMessage(Message):
 
         Returns: self.shift
         '''
-        pass #delete this line and replace with your code here
+        return self.shift
 
     def get_encrypting_dict(self):
         '''
