1cbeb59e705bf85512a26883da6558c801eb023b
Charles Milam <charles.milam@gmail.com>
2016-03-06 15:02:57 -0600

    Implement init method for adoption center. Stores parameters as a dictionary.

--- python-600x/wk7/ps7_skeleton.py
+++ python-600x/wk7/ps7_skeleton.py
@@ -8,33 +8,36 @@ class AdoptionCenter:
     species stored, the location, and the name. It also has a method to adopt a pet.
     """
     def __init__(self, name, species_types, location):
-        # Your Code Here
+        self.adoption_center = {}
+        self.adoption_center['name'] = name
+        self.adoption_center['species_types'] = species_types
+        self.adoption_center['location'] = location
     def get_number_of_species(self, animal):
-        # Your Code Here 
+        # Your Code Here
     def get_location(self):
-        # Your Code Here 
+        # Your Code Here
     def get_species_count(self):
-        # Your Code Here 
+        # Your Code Here
     def get_name(self):
-        # Your Code Here 
+        # Your Code Here
     def adopt_pet(self, species):
-        # Your Code Here 
+        # Your Code Here
 
 
 class Adopter:
-    """ 
+    """
     Adopters represent people interested in adopting a species.
     They have a desired species type that they want, and their score is
     simply the number of species that the shelter has of that species.
     """
     def __init__(self, name, desired_species):
-        # Your Code Here 
+        # Your Code Here
     def get_name(self):
-        # Your Code Here 
+        # Your Code Here
     def get_desired_species(self):
-        # Your Code Here 
+        # Your Code Here
     def get_score(self, adoption_center):
-        # Your Code Here 
+        # Your Code Here
 
 
 
@@ -72,8 +75,8 @@ class MedicatedAllergicAdopter(AllergicAdopter):
     """
     A MedicatedAllergicAdopter is extremely allergic to a particular species
     However! They have a medicine of varying effectiveness, which will be given in a dictionary
-    To calculate the score for a specific adoption center, we want to find what is the most allergy-inducing species that the adoption center has for the particular MedicatedAllergicAdopter. 
-    To do this, first examine what species the AdoptionCenter has that the MedicatedAllergicAdopter is allergic to, then compare them to the medicine_effectiveness dictionary. 
+    To calculate the score for a specific adoption center, we want to find what is the most allergy-inducing species that the adoption center has for the particular MedicatedAllergicAdopter.
+    To do this, first examine what species the AdoptionCenter has that the MedicatedAllergicAdopter is allergic to, then compare them to the medicine_effectiveness dictionary.
     Take the lowest medicine_effectiveness found for these species, and multiply that value by the Adopter's calculate score method.
     """
     # Your Code Here, should contain an __init__ and a get_score method.
@@ -94,16 +97,15 @@ class SluggishAdopter(Adopter):
     """
     # Your Code Here, should contain an __init__ and a get_score method.
 
-    
+
 def get_ordered_adoption_center_list(adopter, list_of_adoption_centers):
     """
     The method returns a list of an organized adoption_center such that the scores for each AdoptionCenter to the Adopter will be ordered from highest score to lowest score.
     """
-    # Your Code Here 
+    # Your Code Here
 
 def get_adopters_for_advertisement(adoption_center, list_of_adopters, n):
     """
     The function returns a list of the top n scoring Adopters from list_of_adopters (in numerical order of score)
     """
-    # Your Code Here 
-
+    # Your Code Here
