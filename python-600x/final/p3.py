5c982352bc7583f3e0a6cc69d5b67c986816ffcf
Charles Milam <charles.milam@gmail.com>
2016-03-13 10:58:11 -0500

    Initial commit of problem 3 from provided stub.

---
+++ python-600x/final/p3.py
@@ -0,0 +1,6 @@
+def dict_invert(d):
+    '''
+    d: dict
+    Returns an inverted dictionary. The inverse of a dictionary d is another dictionary whose keys are the unique dictionary values in d. The value for a key in the inverse dictionary is a sorted list of all keys in d that have the same value in d.
+    '''
+    
