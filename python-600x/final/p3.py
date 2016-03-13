c2bae05c4bcfaac0125329bb3fa7e6ab83e8924f
Charles Milam <charles.milam@gmail.com>
2016-03-13 11:35:20 -0500

    Implement dict_invert. Add try/except to add values of parameter as keys to new dict. Return new dict.

--- python-600x/final/p3.py
+++ python-600x/final/p3.py
@@ -3,4 +3,26 @@ def dict_invert(d):
     d: dict
     Returns an inverted dictionary. The inverse of a dictionary d is another dictionary whose keys are the unique dictionary values in d. The value for a key in the inverse dictionary is a sorted list of all keys in d that have the same value in d.
     '''
-    
+    inverted_dict = {}
+
+    for item in d:
+        try:
+            item_list = inverted_dict[d[item]]
+            item_list.append(item)
+            inverted_dict[d[item]] = item_list
+        except:
+            inverted_dict[d[item]] = [item]
+
+    return inverted_dict
+
+ # Test 1
+print 'Expected: {10: [1], 20: [2], 30: [3]}'
+print 'Actual:', dict_invert({1:10, 2:20, 3:30})
+
+# Test 2
+print 'Expected: {10: [1], 20: [2], 30: [3, 4]}'
+print 'Actual:', dict_invert({1:10, 2:20, 3:30, 4:30})
+
+# Test 3
+print 'Expected: {True: [0, 2, 4]}'
+print 'Actual:', dict_invert({4:True, 2:True, 0:True})
