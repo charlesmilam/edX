0e4312f0247e56aa90d7303b61ee786d5a065089
Charles Milam <charles.milam@gmail.com>
2016-03-13 13:19:31 -0500

    Implement getSublists, and add tests.

--- python-600x/final/p4p1.py
+++ python-600x/final/p4p1.py
@@ -2,22 +2,20 @@ def getSublists(L, n):
     sublists = []
     start_idx = 0
 
-    while len(L[start_idx:n]) == n:
-        sublists.append(L[start_idx:n])
+    while len(L[start_idx:start_idx + n]) == n:
+        sublists.append(L[start_idx:start_idx + n])
         start_idx += 1
-    # if len(L) < n:
-    #     # sublists.append(L[start_idx:n])
-    #     return sublists
-    # else:
-    #     sublists.append(L[start_idx:n])
-    #     start_idx += 1
-    #     getSublists(L[start_idx:], n)
 
-        # return sublists
     return sublists
 
 # Test 1
 L = [10, 4, 6, 8, 3, 4, 5, 7, 7, 2]
 n = 4
 print 'Expected: [[10, 4, 6, 8], [4, 6, 8, 3], [6, 8, 3, 4], [8, 3, 4, 5], [3, 4, 5, 7], [4, 5, 7, 7], [5, 7, 7, 2]]'
+print 'Actual:', getSublists(L, n)
+
+# Test 2
+L = [1, 1, 1, 1, 4]
+n = 2
+print 'Expected: [[1, 1], [1, 1], [1, 1], [1, 4]]'
 print 'Actual:', getSublists(L, n)
