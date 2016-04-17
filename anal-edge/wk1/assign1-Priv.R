01c762c82c79a6b4aa6620594a9bb1cdf65f69fe
Charles Milam <charles.milam@gmail.com>
2016-04-17 14:51:28 -0500

    Complete problem set 3 for optional problem of week 1.

--- anal-edge/wk1/assign1-Priv.R
+++ anal-edge/wk1/assign1-Priv.R
@@ -18,5 +18,21 @@ limited = subset(poll, poll$Internet.Use == 1 | poll$Smartphone == 1)
 # limited = poll[complete.cases(poll[, 1:2]),]
 str(limited)
 
-# determine variables with NA values
+# determine variables with NA values and average number of personal information
 summary(limited)
+
+# determine number of interviewees with 0 for Info.On.Internet
+# and number of interviewees with 11
+table(limited$Info.On.Internet)
+
+# determine proportion of Worry.About.Info
+table(limited$Worry.About.Info == 1, limited$Worry.About.Info)
+
+# determine proportion of Anonymity
+table(limited$Anonymity.Possible == 1, limited$Anonymity.Possible)
+
+# determine proportion of Tried.Masking
+table(limited$Tried.Masking.Identity == 1, limited$Tried.Masking.Identity)
+
+# determine proportion of Privacy.Law
+table(limited$Privacy.Laws.Effective)
