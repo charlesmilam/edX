79929b472a0430b3a6f31685ce83b8e191e2775f
Charles Milam <charles.milam@gmail.com>
2016-04-16 22:44:27 -0500

    Complete problem set 1

--- anal-edge/wk1/assign1-Stocks.R
+++ anal-edge/wk1/assign1-Stocks.R
@@ -1,2 +1,36 @@
 # Assignment 1 Stocks Data
 
+# Problem 1 - Summary Statistics
+
+# create data frames from provided data files
+IBM = read.csv("IBMStock.csv")
+GE = read.csv("GEStock.csv")
+ProcterGamble = read.csv("ProcterGambleStock.csv")
+CocaCola = read.csv("CocaColaStock.csv")
+Boeing = read.csv("BoeingStock.csv")
+
+# convert dates to Date objects
+IBM$Date = as.Date(IBM$Date, "%m/%d/%y")
+GE$Date = as.Date(GE$Date, "%m/%d/%y")
+CocaCola$Date = as.Date(CocaCola$Date, "%m/%d/%y")
+ProcterGamble$Date = as.Date(ProcterGamble$Date, "%m/%d/%y")
+Boeing$Date = as.Date(Boeing$Date, "%m/%d/%y")
+
+# view structure of data frames to determine the number of obs
+str(IBM)
+str(GE)
+str(CocaCola)
+str(ProcterGamble)
+str(Boeing)
+
+# determine the earlist and latest year listed in the dataets
+# as well as mean, min, max, and median of the various stocks
+summary(IBM)
+summary(GE)
+summary(CocaCola)
+summary(ProcterGamble)
+summary(Boeing)
+
+# determine standard deviaion of Procter & Gamble
+sd(ProcterGamble$StockPrice)
+
