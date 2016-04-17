481ccc331d745abc7ce9954779da00fb5c459fc2
Charles Milam <charles.milam@gmail.com>
2016-04-17 15:17:12 -0500

    Complete problem set 4 for optional problem of week 1 assignment.

--- anal-edge/wk1/assign1-Priv.R
+++ anal-edge/wk1/assign1-Priv.R
@@ -36,3 +36,22 @@ table(limited$Tried.Masking.Identity == 1, limited$Tried.Masking.Identity)
 
 # determine proportion of Privacy.Law
 table(limited$Privacy.Laws.Effective)
+
+# build histogram for ages
+hist(limited$Age)
+
+# determine overlapping points of age and info.on.internet
+max(table(limited$Age, limited$Info.On.Internet))
+
+# experiment with the jitter command
+jitter(c(1, 2, 3))
+jitter(c(1, 2, 3))
+
+# determine relationship between age and info.on via plot with jiter
+plot(jitter(limited$Age), jitter(limited$Info.On.Internet))
+
+# determine average of info.on for smartphone and non smartphone users
+tapply(limited$Info.On.Internet, limited$Smartphone == 1, mean)
+
+# determine avergae of tried.masking for smartphone or non smartphone users
+tapply(limited$Tried.Masking.Identity, limited$Smartphone, mean, na.rm = TRUE)
