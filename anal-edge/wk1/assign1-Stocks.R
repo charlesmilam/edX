6724223ff07067b1a2d89d7cfa3994e0b634098a
Charles Milam <charles.milam@gmail.com>
2016-04-16 23:47:37 -0500

    Complete problem set 4

--- anal-edge/wk1/assign1-Stocks.R
+++ anal-edge/wk1/assign1-Stocks.R
@@ -60,3 +60,18 @@ lines(Boeing$Date[301:432], Boeing$StockPrice[301:432], type="l", col="green", y
 # add ablines to aid in isolating a subset of dates
 abline(v = as.Date(c("1997-09-01")), lwd = 2)
 abline(v = as.Date(c("1997-11-30")), lwd = 2)
+
+# Problem 4 - Monthly Trends
+
+# determine the mean stock price of IBM sorted by months, and use to compare
+# against the overall average stock price
+tapply(IBM$StockPrice, months(IBM$Date), mean)
+summary(IBM)
+
+# determine the mean stock price for the other stocks
+tapply(CocaCola$StockPrice, months(CocaCola$Date), mean)
+tapply(GE$StockPrice, months(GE$Date), mean)
+tapply(ProcterGamble$StockPrice, months(ProcterGamble$Date), mean)
+tapply(Boeing$StockPrice, months(Boeing$Date), mean)
+
+
