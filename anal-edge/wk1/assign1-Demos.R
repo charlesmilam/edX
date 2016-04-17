61d0c9fd91951f44837d5cf749371f0054cf4e31
Charles Milam <charles.milam@gmail.com>
2016-04-17 01:40:01 -0500

    Complete problems set 3 of Demos

--- anal-edge/wk1/assign1-Demos.R
+++ anal-edge/wk1/assign1-Demos.R
@@ -18,3 +18,31 @@ sort(table(CPS$State))
 # determine races that identify as of Hispanic ethnicity
 Hispanics = CPS$Race[CPS$Hispanic == 1]
 table(Hispanics)
+
+# Problem 2 - Evaluating Missing Values
+
+# determine columns with NAs
+summary(CPS)
+
+# determine if patern exists for NAs
+table(CPS$Region, is.na(CPS$Married))
+table(CPS$Sex, is.na(CPS$Married))
+table(CPS$Age, is.na(CPS$Married))
+table(CPS$Citizenship, is.na(CPS$Married))
+
+# determine states with all interviewees living in a non-metropolitan area and
+# vice versa
+table(CPS$State, is.na(CPS$MetroAreaCode))
+
+# determine region which has largest proportion of interviewees living in non-metro areas
+prop.table(CPS$Region, is.na(CPS$MetroAreaCode))
+
+# determine state proportions for metro/non-metro interviewees
+tapply(c(is.na(CPS$MetroAreaCode)), CPS$State,  mean)
+
+# Problem 3 - Integrating Metropolitan Area Data
+
+# determine number of obs for Metro Area Codes and Country Codes
+str(MetroCodes)
+str(CountryCodes)
+
