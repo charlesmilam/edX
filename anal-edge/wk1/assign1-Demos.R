fe0eb72b7c1a6ede822e59900c02b2af92284366
Charles Milam <charles.milam@gmail.com>
2016-04-17 00:27:37 -0500

    Complete problem set 1 of Demographics

--- anal-edge/wk1/assign1-Demos.R
+++ anal-edge/wk1/assign1-Demos.R
@@ -5,3 +5,16 @@ CPS = read.csv("CPSData.csv")
 MetroCodes = read.csv("MetroAreaCodes.csv")
 CountryCodes = read.csv("CountryCodes.csv")
 
+# Problem 1 - Loading and Summarizing the Dataset
+summary(CPS)
+str(CPS)
+
+# table of states sorted
+sort(table(CPS$State))
+
+# determine proportion of US citizens
+(length(which(CPS$Citizenship == "Citizen, Native")) + length(which(CPS$Citizenship == "Citizen, Naturalized"))) / length(CPS$Citizenship)
+
+# determine races that identify as of Hispanic ethnicity
+Hispanics = CPS$Race[CPS$Hispanic == 1]
+table(Hispanics)
