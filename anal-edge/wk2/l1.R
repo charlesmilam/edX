8e924f1e240ff1a9a171c92b7149a0a2976eea6e
Charles Milam <charles.milam@gmail.com>
2016-04-28 21:42:03 -0500

    Initial commit of R script for lecture 1 of week 2 of AE. Set up import of data and check of structure and summary.

---
+++ anal-edge/wk2/l1.R
@@ -0,0 +1,11 @@
+# exercises for lecture 1
+
+# load the wine data
+wine = read.csv("wine.csv")
+
+# check the structure of the wine data frame
+str(wine)
+
+# check the summary info
+summary(wine)
+
