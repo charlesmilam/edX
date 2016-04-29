b5bc68b95c2e678e68df899f8b5f182235b9fab1
Charles Milam <charles.milam@gmail.com>
2016-04-28 21:54:48 -0500

    Wk2 lecture, add linear regression model (model1).

--- anal-edge/wk2/l1.R
+++ anal-edge/wk2/l1.R
@@ -9,3 +9,21 @@ str(wine)
 # check the summary info
 summary(wine)
 
+# create linear regression model for AGST
+model1 = lm(Price ~ AGST, data = wine)
+
+# check summary of model1
+summary(model1)
+
+# view residuals of model1
+model1$residuals
+
+# compute the SSE and view it
+SSE = sum(model1$residuals^2)
+SSE
+
+# create new model with additional ind variables
+model2 = lm(Price ~ AGST + HarvestRain, data = wine)
+
+# view summary of model2
+summary(model2)
