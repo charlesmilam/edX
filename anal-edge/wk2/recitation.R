0f4de10a3de0a00a5aba1db093b30982bfd71c09
Charles Milam <charles.milam@gmail.com>
2016-04-30 15:44:07 -0500

    W2 R, Import in training data. Add variable for points difference. Plot the wins vs. points diff. Create model based on same.

--- anal-edge/wk2/recitation.R
+++ anal-edge/wk2/recitation.R
@@ -1,2 +1,19 @@
 # exercises for recitation
 
+# import the training data, view its structure and summary
+nba = read.csv("NBA_train.csv")
+str(nba)
+summary(nba)
+
+# view table to determine number of wins needed to make playoffs
+table(nba$W, nba$Playoffs)
+
+# create variable to hold points difference
+nba$pts_diff = nba$PTS - nba$oppPTS
+
+# create scatter plot of pts diff vs. wins
+plot(nba$pts_diff, nba$W)
+
+# create regression model for wins and view its summary
+wins_model = lm(W ~ pts_diff, data = nba)
+summary(wins_model)
