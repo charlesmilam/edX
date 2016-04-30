3a78f162dda507c2246a59256e5e9fe3babe6a21
Charles Milam <charles.milam@gmail.com>
2016-04-29 22:48:06 -0500

    W2 L2, Add subset of data for years prior to 2002. Add plot of wins vs. run difference. Add model for wins with ind variable of RD.

--- anal-edge/wk2/l2.R
+++ anal-edge/wk2/l2.R
@@ -5,3 +5,24 @@ baseball = read.csv("baseball.csv")
 
 # view the structure of baseball
 str(baseball)
+
+# create subset of baseball, include only years before 2002
+moneyball = subset(baseball, baseball$Year < 2002)
+
+# view structure of moneyball
+str(moneyball)
+
+# create new variable for moneyball to hold run difference
+moneyball$RD = moneyball$RS - moneyball$RA
+
+# view structure of moneyball to see new variable
+str(moneyball)
+
+# create plot of run difference vs. wins
+plot(moneyball$RD, moneyball$W)
+
+# create model based on ind variable RD
+wins_reg = lm(W ~ RD, data = moneyball)
+
+# view summary of wins_reg
+summary(wins_reg)
