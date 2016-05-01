727ec6aa00e01f8089fba7fa4cba436049c3ad20
Charles Milam <charles.milam@gmail.com>
2016-05-01 13:00:13 -0500

    W2 PS1, load the data and split into two variables, one for training the model, and one for testing the model.

--- anal-edge/wk2/climate.R
+++ anal-edge/wk2/climate.R
@@ -1,2 +1,15 @@
 # exercises for week 2 homework, problem set 1
 
+# load the provided climate change data
+climate = read.csv("climate_change.csv")
+
+# create a trainging set of the data
+climate_train = climate[climate$Year < 2007, ]
+# and a test set of the remaining years
+climate_test = climate[climate$Year >= 2007, ]
+
+# view structure and summary of climate train and test
+str(climate_train)
+summary(climate_train)
+str(climate_test)
+summary(climate_test)
